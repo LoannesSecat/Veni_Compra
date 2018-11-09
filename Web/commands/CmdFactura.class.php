b33ffb89367253c55731896f599ec69c2ed59ed2
MamianS12 <38766984+MamianS12@users.noreply.github.com>
2018-11-09 13:36:28 -0500

    Add files via upload

---
+++ Web/commands/CmdFactura.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package vc2
+ * @subpackage Commands
+ */
+
+require_once "WebRequest.class.php";
+/**
+ * Comando para mostrar la interfaz de la tabla: mus_usuarios
+ * @author SpyroFrameWork
+ * @copyright Spyro Solutions
+ */
+class CmdFactura {
+
+	/**
+	 * @return boolean 
+	 */
+	function execute()
+    {
+        //delete session for forms
+        WebSession::unsetPropertyForms();
+        return "success";  
+    }
+
+}
+
+?>	
